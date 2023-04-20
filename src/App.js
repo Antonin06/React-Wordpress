f52c16f5b7c36fe624e297f6d7037b3fba7a0306
Antonin06 <avon.antonin@gmail.com>
2023-04-20 12:15:06 +0200

    [*] Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import client from "./config/graphql/apollo";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ApolloProvider client={client}>
+        <Switch>
+          {/*<Route exact path="/" component={HomePage} />*/}
+          {/*<Route path="/blog/:slug" component={PostPage} />*/}
+        </Switch>
+      </ApolloProvider>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+    uri: '',
+    cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
